2270aa3888aac0fc03b77b0bcd3c8f8f11578ccb
hyper <hyper_han@outlook.com>
2021-11-13 20:53:54 +0800

    myapp

---
+++ myapp/Shared/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  Shared
+//
+//  Created by Hyper on 2021/11/13.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
